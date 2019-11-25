e15866d75c19de4fcf3e37fb92acb3a7c8ecbc2d
lundergust <joey.leaman@yahoo.com>
2019-11-24 22:15:00 -0600

    Beginnings

---
+++ KingsandFriends/Kings and Friends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kings and Friends
+//
+//  Created by lundergust on 11/24/19.
+//  Copyright © 2019 lundergust. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
