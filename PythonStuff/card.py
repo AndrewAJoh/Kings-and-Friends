4280bd4b81c2e3e0531c62b5047a02e99c289666
lundergust <joey.leaman@yahoo.com>
2019-12-08 19:42:31 -0600

    "things"

---
+++ PythonStuff/card.py
@@ -0,0 +1,7 @@
+class Card:
+    def __init__(self, value, suit, color):
+        super().__init__()
+        self.value = value
+        self.suit = suit
+        self.color = color
+        self.position = int
